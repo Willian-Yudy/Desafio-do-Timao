b06f494835e117ed9bdb4ffc518ec8baa34bdab6
Willian <111684744+Willian-Yudy@users.noreply.github.com>
2025-08-14 00:47:24 +1000

    Update QuizGame.cpp
    Signed-off-by: Willian <111684744+Willian-Yudy@users.noreply.github.com>

--- src/QuizGame.cpp
+++ src/QuizGame.cpp
@@ -1,1 +1,39 @@
+#include "../include/QuizGame.h"
+#include <iostream>
 
+QuizGame::QuizGame(string nome) : jogador(nome) {}
+
+void QuizGame::adicionarPergunta(unique_ptr<BaseQuestion> q) {
+    perguntas.push_back(move(q));
+}
+
+void QuizGame::iniciar() {
+    jogador.reset();
+    cout << "\n==============================\n";
+    cout << "    DESAFIO DO TIMÃO 🦅\n";
+    cout << "==============================\n";
+    cout << "Acertar 5 = Gavião | Errar 3 = Palmeirense 🐖\n";
+
+    for (auto &p : perguntas) {
+        p->mostrar(false);
+        char resp = static_cast<MultipleChoiceQuestion*>(p.get())->pegarResposta(p->temDica());
+        
+        if (resp == 'H') {
+            p->mostrar(true);
+            resp = static_cast<MultipleChoiceQuestion*>(p.get())->pegarResposta(false);
+        }
+
+        if (p->verificarResposta(resp)) {
+            cout << "✅ Correto!\n";
+            jogador.addEstrela();
+        } else {
+            cout << "❌ Errado!\n";
+            jogador.addErro();
+        }
+
+        if (jogador.getEstrelas() >= 5 || jogador.getErros() >= 3) break;
+    }
+
+    cout << "\n===== RESULTADO FINAL =====\n";
+    cout << jogador.tituloFinal() << "\n";
+}
